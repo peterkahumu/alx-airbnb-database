a637d671df72c349423cc155e6e2f59c6900dbc1
peterkahumu <muhumukip@gmail.com>
2025-05-15 23:47:39 +0300

    Updated the query to include EXPLAIN

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,4 +1,16 @@
-SELECT 
+EXPLAIN SELECT 
+	RANK() OVER (ORDER BY COUNT(*) DESC) AS "Rank",
+	CONCAT(u.first_name, ' ', u.last_name) AS "Name",
+	COUNT(*) AS "Total Bookings"
+FROM booking b
+INNER JOIN users u ON b.user_id = u.user_id
+GROUP BY "Name"
+ORDER BY "Total Bookings" DESC;
+
+CREATE INDEX IF NOT EXISTS index01_users ON users(user_id, first_name, last_name);
+CREATE INDEX IF NOT EXISTS index01_booking ON booking(booking_id);
+
+EXPLAIN SELECT 
 	RANK() OVER (ORDER BY COUNT(*) DESC) AS "Rank",
 	CONCAT(u.first_name, ' ', u.last_name) AS "Name",
 	COUNT(*) AS "Total Bookings"
