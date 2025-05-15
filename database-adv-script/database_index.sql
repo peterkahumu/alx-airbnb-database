09fd8203a6dadc73a55f8318f8c6eb90ae0c9b19
peterkahumu <muhumukip@gmail.com>
2025-05-15 23:49:11 +0300

    Updated the query to include EXPLAIN ANALYZE

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,4 +1,4 @@
-EXPLAIN SELECT 
+EXPLAIN ANALYZE SELECT 
 	RANK() OVER (ORDER BY COUNT(*) DESC) AS "Rank",
 	CONCAT(u.first_name, ' ', u.last_name) AS "Name",
 	COUNT(*) AS "Total Bookings"
@@ -10,7 +10,7 @@ ORDER BY "Total Bookings" DESC;
 CREATE INDEX IF NOT EXISTS index01_users ON users(user_id, first_name, last_name);
 CREATE INDEX IF NOT EXISTS index01_booking ON booking(booking_id);
 
-EXPLAIN SELECT 
+EXPLAIN ANALYZE SELECT 
 	RANK() OVER (ORDER BY COUNT(*) DESC) AS "Rank",
 	CONCAT(u.first_name, ' ', u.last_name) AS "Name",
 	COUNT(*) AS "Total Bookings"
