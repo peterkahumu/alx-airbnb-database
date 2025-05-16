da6d8eec3d9a48aa37ae4eacd9c2b396e73c9cb8
peterkahumu <muhumukip@gmail.com>
2025-05-16 22:32:29 +0300

    Included Explain in the perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -10,7 +10,7 @@ CREATE INDEX IF NOT EXISTS idx_property_property_id ON property(property_id);
 CREATE INDEX IF NOT EXISTS idx_booking_booking_id ON booking(booking_id);
 CREATE INDEX IF NOT EXISTS idx_payment_payment_id ON payment(payment_id);
 
-SELECT 
+EXPLAIN ANALYZE SELECT 
 	b.booking_id as "Booking ID",
 	b.status as "Status",
 	CONCAT(u.first_name, ' ', u.last_name) as "Full Name",
