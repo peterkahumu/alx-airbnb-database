1704fb1368992df7d4f237af9234f12f5da4c0d8
peterkahumu <muhumukip@gmail.com>
2025-05-16 22:30:00 +0300

    Updated the perfomance query

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,3 +1,15 @@
+
+-- Optimize the Query
+
+CREATE INDEX IF NOT EXISTS idx_booking_user_id ON booking(user_id);
+CREATE INDEX IF NOT EXISTS idx_booking_property_id ON booking(property_id);
+CREATE INDEX IF NOT EXISTS idx_property_host_id ON property(host_id);
+CREATE INDEX IF NOT EXISTS idx_payment_bookin_id ON payment(booking_id);
+CREATE INDEX IF NOT EXISTS idx_users_user_id ON users(user_id);
+CREATE INDEX IF NOT EXISTS idx_property_property_id ON property(property_id);
+CREATE INDEX IF NOT EXISTS idx_booking_booking_id ON booking(booking_id);
+CREATE INDEX IF NOT EXISTS idx_payment_payment_id ON payment(payment_id);
+
 SELECT 
 	b.booking_id as "Booking ID",
 	b.status as "Status",
@@ -13,4 +25,6 @@ FROM booking b
 INNER JOIN users u ON u.user_id = b.user_id
 INNER JOIN property p ON p.property_id = b.property_id
 INNER JOIN users h ON h.user_id = p.host_id
-LEFT JOIN payment py ON b.booking_id = py.booking_id;
+INNER JOIN payment py ON b.booking_id = py.booking_id
+WHERE b.status = 'confirmed' 
+AND py.payment_method = 'paypal';
